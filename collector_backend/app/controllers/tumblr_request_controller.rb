819f32dcdd9729b2ca920c9edca5e01c4652ae41
Amanda Nagai <amanda.nagai@gmail.com>
2018-10-02 22:22:18 -0700

    searching with blog names dynamically

--- collector_backend/app/controllers/tumblr_request_controller.rb
+++ collector_backend/app/controllers/tumblr_request_controller.rb
@@ -3,9 +3,8 @@ class TumblrRequestController < ApplicationController
   def index
     client = Tumblr::Client.new :consumer_key => 'wqc6FQvXj6AhhegP3uR0ABnBBseCTF6NnnUe4nhcz24XuGfwE7'
 
-    byebug
-    posts_list = client.posts(`#{params.blog_name}.tumblr.com`)
-    # posts_list = client.posts 'thedailyzen.tumblr.com'
+    blog_post = params.require(:blog_name)
+    posts_list = client.posts("#{blog_post}.tumblr.com")
     p '**************************************', client.posts('thedailyzen.tumblr.com')
     render json: posts_list
   end
